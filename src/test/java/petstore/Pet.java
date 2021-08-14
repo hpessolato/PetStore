f17c8f37c56c5f53419d7b819b6ba4b577e1c18b
hpessolato <humbertopessolato@hotmail.com>
2021-08-13 23:58:02 -0300

    Incluida assertivas para o body do teste incluirPet, consultarPet, alterarPet e excluirPet

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -11,24 +11,25 @@ import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
 
 // 3 - Classe
 public class Pet {
     // 3.1 - Atributos
-    String uri = "https://petstore.swagger.io/v2/pet"; // endere√ßo da entidade Pet
+    String uri = "https://petstore.swagger.io/v2/pet"; // endereÁo da entidade Pet
 
-    // 3.2 - M√©todos e Fun√ß√µes
+    // 3.2 - MÈtodos e FunÁıes
     public String lerJson(String caminhoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
     // Incluir - Create - Post
-    @Test // Identifica o m√©todo ou fun√ß√£o como um teste para o TestNG
+    @Test(priority = 1)// Identifica o mÈtodo ou funÁ„o como um teste para o TestNG
     public void incluirPet() throws IOException {
         String jsonBody = lerJson("db/pet1.json");
 
         //Sintaxe Ghirken
-        //Dado - Quando - Ent√£o
+        //Dado - Quando - Ent„o
         //Given - When - Then
 
         given() // Dado
@@ -37,11 +38,90 @@ public class Pet {
                 .body(jsonBody)
         .when() // Quando
                 .post(uri)
-        .then() // Ent√£o
+        .then() // Ent„o
                 .log().all()
                 .statusCode(200)
                 .body("name", is("Lindomar"))
                 .body("status", is("available"))
+                .body("category.name", is ("QWERTY789654"))
+                .body("tags.name", contains("data"))
+        ;
+    }
+
+    /*
+    public void consultarPet(){
+        String petId = "1982030439";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .get(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Lindomar"))
+                .body("category.name", is("QWERTY789654"))
+                .body("status", is("available"))
+        ;
+    }
+    */
+
+    @Test(priority = 2)
+    public void consultarPet() {
+        String petId = "1982030439";
+
+        String token =
+                given()
+                        .contentType("application/json")
+                        .log().all()
+                .when()
+                        .get(uri + "/" + petId)
+                .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body("name", is("Lindomar"))
+                        .body("category.name", is("QWERTY789654"))
+                        .body("status", is("available"))
+                .extract()
+                        .path("category.name")
+        ;
+                System.out.println("O token È " + token);
+    }
+
+    @Test(priority = 3)
+    public void alterarPet() throws IOException {
+        String jsonBody = lerJson("db/pet2.json");
+
+        given() // Dado
+                .contentType("application/json") // comum em API REST - antigas era "text/xml"
+                .log().all()
+                .body(jsonBody)
+        .when() // Quando
+                .put(uri)
+        .then() // Ent„o
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Lindomar"))
+                .body("status", is("sold"))
+        ;
+    }
+
+    @Test(priority = 4)
+    public void excluirPet() {
+        String petId = "1982030439";
+
+        given() // Dado
+                .contentType("application/json") // comum em API REST - antigas era "text/xml"
+                .log().all()
+        .when() // Quando
+                .delete(uri + "/" + petId)
+        .then() // Ent„o
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is(petId))
         ;
     }
 }
